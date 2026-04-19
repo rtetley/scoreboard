3f69c51760b2e99186c8afe5d4b02fbe2dcbb2cd
Romain Tetley <romain.tetley@inria.fr>
2026-04-19 12:27:29 +0200

    Fix blank page on GitHub Pages: set Router basename from BASE_URL

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ function Home() {
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/display" element={<Display />} />
